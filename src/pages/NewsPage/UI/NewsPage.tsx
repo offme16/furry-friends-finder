686b6adaf49cbffb229d4da79954b55fa3d52d3a
Vladimir Malyshev <malyshev200522@gmail.com>
2024-06-28 16:01:34 +0300

    pagination, mapping news in grid

--- src/pages/NewsPage/UI/NewsPage.tsx
+++ src/pages/NewsPage/UI/NewsPage.tsx
@@ -2,23 +2,75 @@ import { getNews, getNewsData, offsetNewsPage } from "enteties/NewsFeed";
 import { useSelector } from "react-redux";
 import cls from './NewsPage.module.scss';
 import { useAppDispatch } from "shared/lib/hooks/useAppDispatch";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { News } from "enteties/NewsFeed/model/type/type";
 
 const NewsPage = () => {
     const dispatch = useAppDispatch();
-
-    const offset = useSelector(offsetNewsPage);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [randomNews, setRandomNews] = useState<News[]>([]);;
+    const news = useSelector(getNewsData);
+    const maxcount: number = 6;
+    
 
     useEffect(() => {
-        dispatch(getNews(offset));
-    }, [dispatch])
+        dispatch(getNews({page: currentPage, limit: 6}));
+    }, [dispatch, currentPage])
 
-    const news = useSelector(getNewsData); //получение данных из стора
+    useEffect(() => {
+        if (news) {
+            setRandomNews(shuffleArray(news));
+        }
+    }, [news]);
+
+    const shuffleArray = (array: News[]) : News[] => {
+        let shuffledArray = array.slice();
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+        return shuffledArray;
+    };
+
+    const handleNextPage = () => {
+        if (news && news.length >= maxcount) {
+            setCurrentPage(prevPage => prevPage + 1);
+        }
+        
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(prevPage => prevPage - 1);
+        }
+    };
 
     return (
-        <div className={cls.container}>
-
+        <div className={cls.NewsPage}>
+            {randomNews && randomNews.length > 0 ? (
+                <div className = {cls.NewsGrid}>
+                {randomNews.map((item) => (
+                    <div key = {item.id} className = {cls.NewsItem}>
+                        <img src = {item.url} alt = {`news-${item.id}`} className = {cls.NewsImage} />
+                        <p className = {cls.NewsFact}> {item.fact} </p>
+                    </div>
+                ))}    
+                </div>
+            ) : (
+                <p>No news available</p>
+            )}
+        
+            <div className={cls.Pagination}>
+                <button onClick={handlePreviousPage} disabled={currentPage === 1} className={cls.PageButton}>
+                    Назад
+                </button>
+                <button onClick={handleNextPage} disabled= {news && news.length < maxcount} className={cls.PageButton}>
+                    Далее
+                </button>
+            </div>
         </div>
+
     )
 }
+
 export default NewsPage;
