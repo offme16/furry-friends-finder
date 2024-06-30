c776553d711ece644f5e43035dffe52b1af43d41
Damir-Sayfullin <saifullin.d02@mail.ru>
2024-07-01 00:12:32 +0500

    +изменен блок "Контакты"

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -2,6 +2,7 @@ import Button from 'shared/UI/Button/Button';
 import cls from './MainPage.module.scss'
 import Input from 'shared/UI/Input/Input';
 import dog from '../../../shared/assests/dog.svg';
+import dog_2 from '../../../shared/assests/dog_2.png';
 import phone from '../../../shared/assests/phone.svg';
 import email from '../../../shared/assests/email.svg';
 import github from '../../../shared/assests/github.svg';
@@ -21,43 +22,46 @@ const MainPage = () => {
                     <img className={cls.bg_img} src={dog} alt='dog' />
                 </div>
             </section>
-            {/* todo: добавить заголовок "Контакты", картинку
-                и текст что-то вроде "Остались вопросы? Свяжитесь с нами 
-                любым удобным для вас способом или оставьте заявку" */}
-            {/* problem: нужно, чтобы этот всё было выше блоков
-                "Оставьте заявку" и "Свяжитесь с нами". У меня это сделать не получилось :( 
-                Он либо слева этих блоков, либо всё куда-то едет или теряются стили, 
-                а дублировать их не хочется*/}
             <section className={cls.contacts}>
-                <div className={cls.title}>
-                    <h1>Оставьте заявку</h1>
-                    <form className={cls.form}>
-                        <Input type="number" placeholder="Телефон" className={cls.Input} children='Введите номер телефона:'></Input>
-                        <br />
-                        <Input type="email" placeholder="Email" className={cls.Input} children='Вы можете указать Email, если хотите получать рассылку:'></Input>
-                        <br />
-                        <Button type="submit">Отправить</Button>
-                    </form>
+                {/* Нужно стилизовать вот этот блок div */}
+                <div className={cls.contactsTitle}>
+                    <h1>Наши контакты</h1>
+                    <div className={cls.circleBlue}></div>
+                    <img className={cls.petImage} src={dog_2} alt="Dog" />
+                    <div className={cls.circlePurple}>Остались вопросы? Свяжитесь с нами
+                        любым удобным для вас способом или оставьте заявку.</div>
                 </div>
-                <div className={cls.contactBlock}>
-                    <h1>Свяжитесь с нами</h1>
-                    <div className={cls.contactItem}>
-                        <a href="tel:+79999999999">
-                            <img src={phone} className={cls.icon} alt="Phone" />
-                            <p className={cls.contactText}>+7 999 999-99-99</p>
-                        </a>
+                <div className={cls.containers}>
+                    <div className={cls.title}>
+                        <h2>Оставьте заявку</h2>
+                        <form className={cls.form}>
+                            <Input type="number" placeholder="Телефон" className={cls.Input} children='Введите номер телефона:'></Input>
+                            <br />
+                            <Input type="email" placeholder="Email" className={cls.Input} children='Вы можете указать Email, если хотите получать рассылку:'></Input>
+                            <br />
+                            <Button type="submit">Отправить</Button>
+                        </form>
                     </div>
-                    <div className={cls.contactItem}>
-                        <a href="email:FindPets@gmail.com">
-                            <img src={email} className={cls.icon} alt="Phone" />
-                            <p className={cls.contactText}>FindPets@gmail.com</p>
-                        </a>
-                    </div>
-                    <div className={cls.contactItem}>
-                        <a href="https://github.com/offme16/furry-friends-finder">
-                            <img src={github} className={cls.icon} alt="Phone" />
-                            <p className={cls.contactText}>GitHub</p>
-                        </a>
+                    <div className={cls.contactBlock}>
+                        <h2>Свяжитесь с нами</h2>
+                        <div className={cls.contactItem}>
+                            <a href="tel:+79999999999">
+                                <img src={phone} className={cls.icon} alt="Phone" />
+                                <p className={cls.contactText}>+7 999 999-99-99</p>
+                            </a>
+                        </div>
+                        <div className={cls.contactItem}>
+                            <a href="email:FindPets@gmail.com">
+                                <img src={email} className={cls.icon} alt="Phone" />
+                                <p className={cls.contactText}>FindPets@gmail.com</p>
+                            </a>
+                        </div>
+                        <div className={cls.contactItem}>
+                            <a href="https://github.com/offme16/furry-friends-finder">
+                                <img src={github} className={cls.icon} alt="Phone" />
+                                <p className={cls.contactText}>GitHub</p>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
