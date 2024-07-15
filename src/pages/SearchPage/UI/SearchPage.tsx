76582c0826a1ece7ec4576f59b6c6c838a29a9ec
offme16 <offmemyg@gmail.com>
2024-07-15 18:27:27 +0300

    добавил адаптив, обработчик ошибки при поиске

--- src/pages/SearchPage/UI/SearchPage.tsx
+++ src/pages/SearchPage/UI/SearchPage.tsx
@@ -6,16 +6,15 @@ import { useAppDispatch } from "shared/lib/hooks/useAppDispatch";
 import { PetsData, getPets, getUniqueFilterValues } from "enteties/PetsRegistered";
 import { useSelector } from "react-redux";
 import { PetsList } from "widgets/PetsList";
-import { debounce }  from 'lodash';
-
+import { debounce } from 'lodash';
 
 const SearchPage = () => {
     const dispatch = useAppDispatch();
     const PetsInfo = useSelector(PetsData);
     const [inputValue, setInputValue] = useState<string>('');
 
-    const {result, page, limit, isLoading, refs} = PetsInfo;
-    
+    const { result, page, limit, isLoading, refs, error } = PetsInfo;
+
     const [filters, setFilters] = useState({
         city: '',
         genderPet: '',
@@ -25,20 +24,23 @@ const SearchPage = () => {
     });
 
     useEffect(() => {
-        dispatch(getPets({page, limit, filters}));
-        dispatch(getUniqueFilterValues())
-    },[dispatch, page, filters])
+        dispatch(getPets({ page, limit, filters }));
+        dispatch(getUniqueFilterValues());
+    }, [dispatch, page, filters]);
 
     const handleFilterChange = (filterName: string, value: string) => {
         const updatedFilters = { ...filters, [filterName]: value };
         setFilters(updatedFilters);
         dispatch(getPets({ page: 1, limit, filters: updatedFilters }));
-    };    
+    };
 
-    const debouncedSearch = useCallback(debounce((value: string) => {
-        handleFilterChange('city', value);
-    }, 500), [filters]);
-
+    const debouncedSearch = useCallback(
+        debounce((value: string) => {
+            handleFilterChange('city', value);
+        }, 500),
+        [filters]
+    );
+    
     const handleCityInputChange = (value: string) => {
         setInputValue(value);
         debouncedSearch(value);
@@ -53,32 +55,38 @@ const SearchPage = () => {
             <h2 className={cls.title}>Домашние питомцы в добрые руки!</h2>
             <div className={cls.content}>
                 <div className={cls.filter}>
-                 <Input
-                    placeholder="Любой город"
-                    onChange={handleCityInputChange}
-                    value={inputValue}>В каком городе поищем?</Input>
+                    <Input
+                        placeholder="Любой город"
+                        onChange={handleCityInputChange}
+                        value={inputValue}
+                    />
                     <Dropdown
                         title="Кого ищете?"
                         items={genderItems}
-                        onChange = {(value) => handleFilterChange('genderPet', value)}
+                        selectedValue={filters.genderPet}
+                        onChange={(value) => handleFilterChange('genderPet', value)}
                     />
                     <Dropdown
                         title="Окрас"
                         items={colorItems}
-                         onChange = {(value) => handleFilterChange('colorPet', value)}
+                        selectedValue={filters.colorPet}
+                        onChange={(value) => handleFilterChange('colorPet', value)}
                     />
                     <Dropdown
                         title="Порода"
                         items={breedItems}
+                        selectedValue={filters.breedPet}
                         onChange={(value) => handleFilterChange('breedPet', value)}
                     />
                 </div>
                 <div className={cls.petsList}>
-                    <PetsList result={result} page={page} limit={limit} isLoading={isLoading} />
+                    {error ? <div className={cls.error}><p>К сожалению, по вашему запросу ничего не найдено.</p>
+                        <p>Попробуйте изменить параметры фильтра или воспользоваться другим поисковым запросом.</p>
+                        </div> : <PetsList result={result} page={page} limit={limit} isLoading={isLoading} />} 
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default SearchPage;
--- src/shared/UI/Dropdown/Dropdown.tsx
+++ src/shared/UI/Dropdown/Dropdown.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import cls from './Dropdown.module.scss';
 import arrow_right from '../../assests/right_arrow_icon.svg';
 
 interface DropdownProps {
     title: string;
     items: Array<{ label: string; value: string }>;
+    selectedValue: string;
     onChange: (value: string) => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ title, items, onChange }) => {
+const Dropdown: React.FC<DropdownProps> = ({ title, items, selectedValue, onChange }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [selected, setSelected] = useState<string>(selectedValue);
+
+    useEffect(() => {
+        setSelected(selectedValue);
+    }, [selectedValue]);
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
@@ -17,20 +23,27 @@ const Dropdown: React.FC<DropdownProps> = ({ title, items, onChange }) => {
 
     const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.checked ? event.target.value : '';
+        setSelected(value);
         onChange(value);
     };
 
     return (
         <div className={cls.dropdown}>
             <div className={cls.dropdownHeader} onClick={toggleDropdown}>
-                <img src={arrow_right} alt='arrow' className={`${cls.dropdownHeader_arrow} ${isOpen ? cls.dropdownHeader_arrow_open : ''}`}/>
+                <img src={arrow_right} alt='arrow' className={`${cls.dropdownHeader_arrow} ${isOpen ? cls.dropdownHeader_arrow_open : ''}`} />
                 {title}
             </div>
             {isOpen && (
                 <div className={cls.dropdownList}>
                     {items.map((item) => (
                         <label key={item.value}>
-                            <input type="checkbox" name={item.value} value={item.value} onChange={handleCheckboxChange}/>
+                            <input
+                                type="checkbox"
+                                name={item.value}
+                                value={item.value}
+                                checked={selected === item.value}
+                                onChange={handleCheckboxChange}
+                            />
                             {item.label}
                         </label>
                     ))}
