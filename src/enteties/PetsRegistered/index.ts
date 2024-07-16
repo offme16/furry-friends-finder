47946e86573d0b6292026814b32279ed548dc085
offme16 <offmemyg@gmail.com>
2024-07-16 15:19:13 +0300

    исправил импорт

--- src/enteties/PetsRegistered/index.ts
+++ src/enteties/PetsRegistered/index.ts
@@ -5,6 +5,7 @@ import { PetsRegisteredReducer } from "./model/slice/PetsRegisteredSlice";
 import { PetsData } from "./model/selectors/PetsData";
 import { PetsRegisteredActions } from "./model/slice/PetsRegisteredSlice";
 import { getUniqueFilterValues } from "./model/service/getUniqueFilterValues";
+import { getPet } from "./model/service/getPet";
 
-export { type PetsRegisteredSchema, type Pets, getPets, PetsRegisteredReducer, PetsData, PetsRegisteredActions, getUniqueFilterValues}
+export { type PetsRegisteredSchema, type Pets, getPets, PetsRegisteredReducer, PetsData, PetsRegisteredActions, getUniqueFilterValues, getPet}
 
